a09dcdfd3e6f3d647f93c1f9e21ad76e8839d431
Nic Mayer <42087659+nicm42@users.noreply.github.com>
2020-08-05 20:03:28 +0100

    Added another thing to the correct guess test

--- cypress/integration/guess_spec.js
+++ cypress/integration/guess_spec.js
@@ -1,3 +1,5 @@
+/// <reference types="cypress" />
+  
 describe('Submit guess', () => {
 	it('Types in a guess and submits it', () => {
     cy.visit('http://localhost:1234');
@@ -10,19 +12,22 @@ describe('Submit guess', () => {
     cy.get('.history').should('exist');
     cy.get('#guess')
       .should('have.value', '');
-    cy.contains('1 50 Cold');
 	})
 
-	it('Adds congrats message if temperature is correct', () => {
+	it('Checks temperature is correct', () => {
 	  cy.visit('http://localhost:1234');
 	  cy.get('#guess')
 	    .type('100')
 	  cy.get('#submit').click();
     cy.contains('1 100 Correct');
     cy.get('.congrats').should('exist');
+    cy.get('#guess')
+      .should('be.disabled');
+    cy.get('#submit')
+      .should('be.disabled');
 	})
 
-	it('Types in a two guesses and submits them', () => {
+it('Types in two guesses and submits them', () => {
 	  cy.visit('http://localhost:1234');
 	  cy.get('#guess')
 	    .type('50')
