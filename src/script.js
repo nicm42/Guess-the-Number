8f90ef54d7b5d2e891b19c5f4ee38f0019ced271
Nic Mayer <42087659+nicm42@users.noreply.github.com>
2020-09-07 20:17:15 +0100

    Updated hsitory-counter width so two digit guesses and one digit guesses are in line

--- src/script.js
+++ src/script.js
@@ -20,8 +20,8 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function generateRandomNumber(min, max){
-  //return Math.floor(Math.random() * (max - min + 1)) + min;
-  return 100; //for testing TODO
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+  //return 100; //for testing
 }
 // eslint-disable-next-line no-undef
 exports.generateRandomNumber = generateRandomNumber;
